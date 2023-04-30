7f50f014716141f8bd74c1e78445ca56c8b1d82e
kapil1603 <kapilkumar1603@gmail.com>
2023-04-30 07:10:26 +0000

    React-CP2-boxes-app

--- boxesApp/index.js
+++ boxesApp/index.js
@@ -1,9 +1,19 @@
-const Box = props => {
+const Box = (props) => {
   //  Write your code here.
-}
+  const { className, content } = props;
+  return <div className={`commonprops ${className}`}>{content}</div>;
+};
 
 const element = (
-  //  Write your code here. 
-)
+  //  Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Boxes</h1>
+    <div className="box-container">
+      <Box className="small" content="Small" />
+      <Box className="medium" content="Medium" />
+      <Box className="large" content="Large" />
+    </div>
+  </div>
+);
 
 ReactDOM.render(element, document.getElementById("root"));
